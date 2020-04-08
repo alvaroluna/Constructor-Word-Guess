dcd3c753b5a70c8d340da533de391d3e101943dc
alvaroluna <luna.alvaro3@gmail.com>
2020-04-07 20:25:57 -0700

    Completed index.js but has errors, saving this current spot

--- index.js
+++ index.js
@@ -62,7 +62,142 @@ function Main() {
 
   // pick random index from americanPresidents list
   var randomIndex = Math.floor(Math.random() * americanPresidents.length);
-  var randomWord = americanPresidents[i];
+  var randomWord = americanPresidents[randomIndex];
+
+  // pass random word through Word() constructor
+  gameWordObj = new Word(randomWord);
+
+  var requireNewWord = false;
+
+  // list of guessed letters
+  var incorrectLetters = [];
+  var correctLetters = [];
+
+  // guesses left
+  var guessesLeft = randomWord.length * 1.5;
+
+  function GameLogic() {
+    // generate new word for Word() constructor if true
+    if (requireNewWord) {
+      // selects random american president from list
+      var randomIndex = Math.floor(Math.random() * americanPresidents.length);
+      var randomWord = americanPresidents[randomIndex];
+
+      // passes random word through Word() constructor
+      gameWordObj = new Word(randomWord);
+
+      requireNewWord = false;
+    }
+
+    // test if letter guessed is correct
+    var wordComplete = [];
+    gameWordObj.objList.forEach(CompleteCheck);
+
+    // letters remaining to be guessed
+    if (wordComplete.includes(false)) {
+      inquirer
+        .prompt([
+          {
+            name: "userInput",
+            type: "input",
+            message: "Guess a letter between A-Z!",
+          },
+        ])
+        .then(function (res) {
+          if (!letterList.includes(res.userInput) || res.userInput.length > 1) {
+            console.log("\nPlease try again!");
+
+            // run game logic again
+            GameLogic();
+          } else {
+            if (
+              incorrectLetters.includes(res.userInput) ||
+              correctLetters.includes(res.userInput) ||
+              res.userInput === ""
+            ) {
+              console.log("\nAlready guessed or nothing entered\n");
+
+              // run game logic again
+              GameLogic();
+            } else {
+              // checks if guess is correct
+              var wordCheckList = [];
+
+              gameWordObj.UserGuess(res.userInput);
+
+              // checks if guess is correct
+              gameWordObj.objList.forEach(WordCheck);
+              if (wordCheckList.join(" ") === wordComplete.join(" ")) {
+                console.log("\nIncorrect!\n");
+
+                incorrectLetters.push(res.userInput);
+                guessesLeft--;
+              } else {
+                console.log("\nCorrect!\n");
+                correctLetters.push(res.userInput);
+              }
+
+              gameWordObj.Log();
+
+              // print guesses left
+              console.log("Guesses left: " + guessesLeft + "\n");
+
+              // print letters guessed already
+              console.log(
+                "Letters guessed: " + incorrectLetters.join(" ") + "\n"
+              );
+
+              // guesses left
+              if (guessesLeft > 0) {
+                // call function
+                GameLogic();
+              } else {
+                console.log("Sorry, you lose!\n");
+
+                RestartGame();
+              }
+
+              function WordCheck(key) {
+                wordCheckList.push(key.guessed);
+              }
+            }
+          }
+        });
+    } else {
+      console.log("YOU WIN!\n");
+
+      RestartGame();
+    }
+
+    function CompleteCheck(key) {
+      wordComplete.push(key.guessed);
+    }
+  }
+
+  function RestartGame() {
+    inquirer
+      .prompt([
+        {
+          name: "restart",
+          type: "list",
+          message: "Would you like to:",
+          choices: ["Play Again", "Exit"],
+        },
+      ])
+      .then(function (res) {
+        if (res.restart === "Play Again") {
+          requireNewWord = true;
+          incorrectLetters = [];
+          correctLetters = [];
+          guessesLeft = randomWord.length * 1.5;
+          GameLogic();
+        } else {
+          return;
+        }
+      });
+  }
+
+  GameLogic();
 }
 
 /////////////////////////
